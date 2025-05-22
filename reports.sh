61e112d61f94209e9be08c40f91728eb7feac888
André Zúquete <andre.zuquete@ua.pt>
2025-05-22 15:05:56 +0100

    Update reports.sh
    Correct name ordering with LC_ALL=pt_PT

--- reports.sh
+++ reports.sh
@@ -9,7 +9,7 @@ nums=`ls -d [1-9]*`
 new_nums=`( for i in $nums; do
     python3 $bindir/name.py < $i/personal | awk '{for (i = 2; i <= NF; i++) printf( "%s-", $i );}'
     echo
-done ) | sort | sed -e "s/[^0-9]*//g"`
+done ) | LC_ALL=pt_PT sort | sed -e "s/[^0-9]*//g"`
 
 for i in $new_nums; do
     echo $i 1>&2
