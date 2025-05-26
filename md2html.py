0f1df26c48ed3bad1cb4fcad87b5bf8d269837a5
André Zúquete <andre.zuquete@ua.pt>
2025-05-26 16:53:21 +0100

    Added <html> </html> boundaries and charset indication
    Sugestão do Sérgio Matos

--- md2html.py
+++ md2html.py
@@ -4,4 +4,7 @@ import markdown
 import sys
 
 html_content = markdown.markdown( sys.stdin.read() )
+
+sys.stdout.write( '<html>\n<meta charset="UTF-8">\n' )
 sys.stdout.write( html_content.replace( '<a href=', '<a target="_blank" href=' ) )
+sys.stdout.write( '\n</html>\n' )
